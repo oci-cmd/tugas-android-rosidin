bd16f5c118d9a907966faef9ef2fa10c178c5cce
Rosidin <rosidin.crb@gmail.com>
2025-12-19 05:16:00 +0700

    Menghapus SplashScreenActivity manual dan mengganti ke Splash Screen API

--- app/src/main/java/com/latihan/tugas/MainActivity.kt
+++ app/src/main/java/com/latihan/tugas/MainActivity.kt
@@ -1,38 +1,39 @@
 package com.latihan.tugas
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
-    // Variabel untuk menahan durasi splash screen (Simulasi Loading)
+
+    // Variabel untuk menahan durasi splash screen
     private var isKeepOn: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        // 1. INSTALASI SPLASH SCREEN
-        // Wajib dipanggil SEBELUM super.onCreate dan setContentView
+        // 1. INSTALASI SPLASH SCREEN (Wajib baris pertama)
         val splashScreen = installSplashScreen()
 
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         // 2. LOGIKA MENAHAN SPLASH SCREEN
-        // Splash akan tetap muncul selama kondisi bernilai 'true'
         splashScreen.setKeepOnScreenCondition {
-            isKeepOn
+            isKeepOn // Selama true, splash screen akan terus muncul
         }
-        // Simulasi proses loading data di background selama 2 detik
-        // Dalam aplikasi nyata, ini bisa diganti dengan proses ambil data API/Database
-        android.os.Handler(android.os.Looper.getMainLooper()).postDelayed({
-            isKeepOn = false // Saat false, Splash hilang dan masuk ke layout utama
-        }, 5000) // 5000 ms = 5 detik
 
+        // 3. Simulasi Loading (5 Detik)
+        Handler(Looper.getMainLooper()).postDelayed({
+            isKeepOn = false // Ubah jadi false agar masuk ke layout utama
+        }, 5000)
 
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
